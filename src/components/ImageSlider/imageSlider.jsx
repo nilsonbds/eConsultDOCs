eaa31fa3ecf005991e801cfe902fffe6262865d4
Nilson <nilsonbds@gmail.com>
2025-06-15 12:57:49 -0300

    nilson

--- src/components/ImageSlider/imageSlider.jsx
+++ src/components/ImageSlider/imageSlider.jsx
@@ -1,17 +1,19 @@
-import React, { useRef } from "react";
-import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
+import React, { useRef, useState } from "react";
+import { LuChevronLeft, LuChevronRight, LuZoomIn, LuX } from "react-icons/lu";
 import "./imageSlider.css";
 
 function ImageSlider() {
+    const [image, setImage] = useState(null);
+
     const images = [
-        { index: 0, src: "/img/prints/slider/000.png", alt: "" },
-        { index: 1, src: "/img/prints/slider/001.png", alt: "" },
-        { index: 2, src: "/img/prints/slider/002.png", alt: "" },
-        { index: 3, src: "/img/prints/slider/003.png", alt: "" },
-        { index: 4, src: "/img/prints/slider/004.png", alt: "" },
-        { index: 5, src: "/img/prints/slider/005.png", alt: "" },
-        { index: 6, src: "/img/prints/slider/006.png", alt: "" },
-        { index: 7, src: "/img/prints/slider/007.png", alt: "" },
+        { index: 0, src: "/img/prints/slider/000.png", alt: "Painel inicial" },
+        { index: 1, src: "/img/prints/slider/001.png", alt: "Atendimentos" },
+        { index: 2, src: "/img/prints/slider/002.png", alt: "Clientes e Grupos" },
+        { index: 3, src: "/img/prints/slider/003.png", alt: "Resultados" },
+        { index: 4, src: "/img/prints/slider/004.png", alt: "Alertas" },
+        { index: 5, src: "/img/prints/slider/005.png", alt: "Consolidação Financeira" },
+        { index: 6, src: "/img/prints/slider/006.png", alt: "Cadastro de Clientes e Grupos" },
+        { index: 7, src: "/img/prints/slider/007.png", alt: "Análise de Score" },
     ];
 
     const scrollInterval = useRef(null);
@@ -37,17 +39,29 @@ function ImageSlider() {
             content.scrollBy({ left: offset, behavior: 'smooth' });
         }
     };
-
+    
     return <div className="image-slider">
         <div className="image-container">
             {
                 images.map((e) => (
-                    <span className="image" key={e.index}>
-                        <img src={e.src} alt={e.alt} />
-                    </span>
+                    <>
+                        <span className="image" key={e.index}>
+                            <span className="zoom"><LuZoomIn onClick={() => { setImage(e) }} /></span>
+                            <img src={e.src} alt={e.alt} />
+                        </span>
+                    </>
                 ))
             }
         </div>
+        {
+            image && (
+                <div className="image-zoom">
+                    <span className="display-title">{image.alt}&nbsp;&nbsp;<LuX onClick={() => { setImage(null) }} /></span>
+                    <img src={image.src} alt={image.alt} />
+                </div>
+
+            )
+        }
         <div className="actions">
             <span
                 className="btn next"
